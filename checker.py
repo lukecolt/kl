75259ed7e62130cbca8ad2cd69e7a6834047d8f6
Lukasz Koltun <50400241+lukecolt@users.noreply.github.com>
2026-02-15 20:37:26 +0100

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,8 +1,13 @@
-from telegram import Bot
 import os
+import asyncio
+from telegram import Bot
 
-bot = Bot(token=os.environ["TELEGRAM_TOKEN"])
-bot.send_message(
-    chat_id=os.environ["TELEGRAM_CHAT_ID"],
-    text="✅ Telegram działa z GitHub Actions!"
-)
+async def main():
+    bot = Bot(token=os.environ["TELEGRAM_TOKEN"])
+    await bot.send_message(
+        chat_id=os.environ["TELEGRAM_CHAT_ID"],
+        text="✅ Telegram działa z GitHub Actions!"
+    )
+
+if __name__ == "__main__":
+    asyncio.run(main())
